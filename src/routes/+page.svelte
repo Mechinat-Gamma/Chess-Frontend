1aa4194e3f89f198380413bbc2ed6597dce622bb
Martin Alebachew <martinalebachew@gmail.com>
2023-12-27 18:16:00 +0200

    Implemented Homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="hero-flex">
+    <div class="title-flex">
+        <p class="title">Find Your Next Game.</p>
+    </div>
+    <img class="chess-board-demo" src="/Images/ChessboardBlurred.svg" alt="Blurred Chess Board" />
+</div>
+
+<style>
+    .hero-flex {
+        display: flex;
+        justify-content: space-between;
+        padding: 50px 120px;
+    }
+
+    .title-flex {
+        display: flex;
+        flex-direction: column;
+        gap: 65px;
+        align-items: baseline;
+    }
+
+    .title {
+        font-size: 105px;
+        font-weight: 700;
+        line-height: 100%;
+        word-spacing: 9999px;
+        margin: 0;
+        margin-top: 50px;
+    }
+
+    .chess-board-demo {
+        width: 554.592px;
+        height: 560.847px;
+    }
+</style>
