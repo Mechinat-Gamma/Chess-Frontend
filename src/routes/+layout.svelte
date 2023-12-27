3d36792ce15111037d5ae55a5293c5c826ae122c
Martin Alebachew <martinalebachew@gmail.com>
2023-12-27 17:49:13 +0200

    Created Layout File
    - Created Svelte layout file for all pages.
    - Moved Inter font code to the layout file and removed style.css.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,24 @@
 <script>
-    import "../style.css"
+    import NavBar from "$lib/NavBar.svelte";
 </script>
 
+<NavBar />
 <slot />
+
+<style>
+    :global(body) {
+        margin: 0;
+        padding: 0;
+    }
+
+    :root {
+        font-family: Inter, sans-serif;
+        font-feature-settings: 'liga' 1, 'calt' 1; /* Chrome-specific bug; Inter font */
+        background-color: #2B292E;
+        color: white;
+    }
+
+    @supports (font-variation-settings: normal) {
+        :root { font-family: InterVariable, sans-serif; }
+    }
+</style>
