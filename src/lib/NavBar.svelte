2a6d9f6e107771b18b69268176c110034d3d6cc4
Martin Alebachew <martinalebachew@gmail.com>
2023-12-27 17:48:39 +0200

    Created Navigation Bar Component

---
+++ src/lib/NavBar.svelte
@@ -0,0 +1,37 @@
+<!-- Navigation Bar Component -->
+
+<div class="navbar">
+    <img src="/Images/ChessIcon.svg" class="navbar-icon" alt="Chess Logo" />
+    <span class="navbar-title">Chess</span>
+    <span class="navbar-item">Home</span>
+    <span class="navbar-item">Play</span>
+    <span class="navbar-item">My Stats</span>
+    <span class="navbar-item">Ranking</span>
+</div>
+
+<style>
+    .navbar {
+        background-color: #1D2022;
+        padding-top: 12px;
+        padding-bottom: 12.75px;
+    }
+
+    .navbar-title {
+        font-size: 26.849px;
+        font-weight: 500;
+        padding-right: 42.71px;
+    }
+
+    .navbar-item {
+        font-size: 18.018px;
+        font-weight: 400;
+        padding-right: 29.62px;
+    }
+
+    .navbar-icon {
+        width: 27.468px;
+        height: 27.799px;
+        padding-right: 11.45px;
+        padding-left: 25.02px;
+    }
+</style>
