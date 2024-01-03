06cc5bdb2f62a7bb3e6a4c8001d4b5d6cad31c39
Martin Alebachew <martinalebachew@gmail.com>
2024-01-03 20:29:50 +0200

    Added Play Now button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
 <div class="hero-flex">
     <div class="title-flex">
         <p class="title">Find Your Next Game.</p>
+        <div class="play-button">Play Now</div>
     </div>
     <Board />
 </div>
@@ -14,7 +15,7 @@
         display: flex;
         justify-content: space-between;
         align-items: center;
-        padding: 50px 120px;
+        padding: 35px 120px;
     }
 
     .title-flex {
@@ -30,5 +31,16 @@
         line-height: 100%;
         word-spacing: 9999px;
         margin-top: 50px;
+    }
+
+    .play-button {
+        background-color: #37548B;
+        font-size: 27.723px;
+        font-weight: 500;
+        border-radius: 42.43px;
+        padding-left: 25px;
+        padding-right: 25px;
+        padding-top: 8px;
+        padding-bottom: 8px;
     }
 </style>
